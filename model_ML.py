43f9a52b1ad5ff8a404b5d77ef7fb3dda0a2148f
nursekti <59988090+nursekti@users.noreply.github.com>
2021-06-01 19:54:02 +0700

    Update model_ML.py

--- model_ML.py
+++ model_ML.py
@@ -21,22 +21,22 @@ from tensorflow.python.keras.saving import saving_utils
 def seq2seq(hidden_size, input_chars, target_chars):
 
     # main model
-    encoder_lstm = LSTM(hidden_size, recurrent_dropout=0.3, return_sequences=True, return_state=False, name='encoder_lstm_1')
+    encoder_lstm = LSTM(hidden_size, recurrent_dropout=0.3, return_sequences=True, return_state=False, name='encoderLSTM1')
     encoder_out = encoder_lstm(Input(shape=(None, input_chars), name='data_encoder'))
-    encoder_lstm = LSTM(hidden_size, recurrent_dropout=0.3, return_sequences=False, return_state=True, name='encoder_lstm_2')
+    encoder_lstm = LSTM(hidden_size, recurrent_dropout=0.3, return_sequences=False, return_state=True, name='encoderLSTM2')
     encoder_out, state_h, state_c = encoder_lstm(encoder_out)
     encoder_states = [state_h, state_c]
 
     # initial state is 'encoder_states'.
-    decoder_lstm = LSTM(hidden_size, dropout=0.3, return_sequences=True, return_state=True, name='decoder_lstm')
+    decoder_lstm = LSTM(hidden_size, dropout=0.3, return_sequences=True, return_state=True, name='decoderLSTM')
     decoder_out, _, _ = decoder_lstm(Input(shape=(None, target_chars), name='data_decoder'), initial_state=encoder_states)
     decoder_dense = Dense(target_chars, activation='softmax', name='decoder_dense')
     decoder_out = decoder_dense(decoder_out)
     
     # Define model
     model = Model(inputs=[encoder_in, decoder_in], outputs=decoder_out)
-    adam = optimizers.Adam(lr=0.001, decay=0.0)
-    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    optimizer = optimizers.Adam(lr=0.001, decay=0.0)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     
     # Define encoder model separately.
     encoder_model = Model(inputs=encoder_in, outputs=encoder_states)
@@ -65,27 +65,22 @@ class CharacterTable(object):
         self.char2index = dict((c, i) for i, c in enumerate(self.chars))
         self.index2char = dict((i, c) for i, c in enumerate(self.chars))
     
-    def encode(self, encoded, rows):
-        """One-hot encode given string encoded
-          encoded: string, to be encoded.
-          rows: Number of rows in the returned one-hot encoding.
-        """
-        encoding = np.zeros((rows, len(self.chars)), dtype=np.float32)
+    def encode(self, encoded, row):
+        # One-hot encode given string encoded
+        encoding = np.zeros((row, len(self.chars)), dtype=np.float32)
         for i, c in enumerate(encoded):
             encoding[i, self.char2index[c]] = 1.0
         return encoding
 
     def decode(self, decoded, argmax=True):
-        """Decode the given vector or 2D array to their character output.
-          decoded: A vector or 2D array of probabilities or one-hot encodings
-          argmax: Whether to find the character index with maximum probability, defaults to `True`.
-        """
+        # Decode the given vector or 2D array to their character output.
         if argmax:
-            indices = decoded.argmax(axis=-1)
+            idx = decoded.argmax(axis=-1)
         else:
-            indices = decoded
-        chars = ''.join(self.index2char[ind] for ind in indices)
-        return indices, chars
+            idx = decoded
+        char = ''
+        chars = char.join(self.index2char[i] for i in idx)
+        return idx, chars
 
     def sample_multinomial(self, preds, temp=1.0):
         # Reshaped to 1D array of shape
@@ -105,13 +100,13 @@ def read_text(data_path, list_of_books):
     return text
 
 def add_spelling_error(token, error_rate):
-    #What to do with the token when countering spelling error
+    # What to do with the token when countering spelling error
     assert(0. <= error_rate < 1.)
     if len(token)<3:
         return token
     rand = np.random.rand()
     random_char = np.random.randint(len(token)) # From first to third quadran
-    probability = error_rate / 4. # From paper, in which 4 different spelling errpr could occur
+    probability = error_rate / 4. # From paper, in which 4 different spelling error could occur
     if rand<probability:
         token = token[:random_char] + np.random.choice(CHARS) + token[random_char + 1:] #tokenize it
     elif probability < rand < probability * 2:
@@ -125,3 +120,26 @@ def add_spelling_error(token, error_rate):
         pass
     return token
 
+def transform(tokens, maxlen, error_rate=0.3, shuffle=True):
+    #Transform tokens into model inputs and targets, also padded to maxlen with EOS character.
+    if shuffle:
+        np.random.shuffle(tokens)
+    encoderTokens = []
+    decoderTokens = []
+    targetTokens = []
+    for token in tokens:
+        encoder = add_speling_erors(token, error_rate=error_rate)
+        # Padded to maxlen
+        encoder += EOS * (maxlen - len(encoder))
+        encoderTokens.append(encoder)
+    
+        decoder = SOS + token
+        decoder += EOS * (maxlen - len(decoder))
+        decoderTokens.append(decoder)
+    
+        target = decoder[1:]
+        target += EOS * (maxlen - len(target))
+        targetTokens.append(target)
+        
+        assert(len(encoder) == len(decoder) == len(target))
+    return encoder_tokens, decoder_tokens, target_tokens
