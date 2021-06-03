31b90d6812656ec95fd05153fd9a1070a522a807
puspaduhita <49947654+puspaduhita@users.noreply.github.com>
2021-06-03 20:51:50 +0700

    Evaluating and Saving the Model to .h5

--- training.py
+++ training.py
@@ -56,3 +56,42 @@ if __name__ == '__main__':
 
     # Train and evaluate.
     for epoch in range(num_epochs):
+        print('Main Epoch {:d}/{:d}'.format(epoch + 1, nb_epochs))
+
+        train_encoder, train_decoder, train_target = transform(vocab, maxlen, error_rate=error_rate, shuffle=True)   
+        # Load the data for training and validation
+        train_loader = load_data(batch(train_encoder, maxlen, input_ctable,train_batch_size, reverse),
+                               batch(train_decoder, maxlen, target_ctable, train_batch_size), 
+                               batch(train_target, maxlen, target_ctable, train_batch_size))
+        val_loader = load_data(batch(val_encoder, maxlen, input_ctable, val_batch_size, reverse),
+                             batch(val_decoder, maxlen, target_ctable, val_batch_size), 
+                             batch(val_target, maxlen, target_ctable, val_batch_size))
+        # Evaluate the model
+        model.fit(train_loader,
+                  steps_per_epoch=train_steps,
+                  epochs=1, verbose=1,
+                  validation_data=val_loader,
+                  validation_steps=val_steps)
+
+        # On epoch end - decode a batch of misspelled tokens from the
+        # validation set to visualize speller performance.
+        nb_tokens = 5
+        input_tokens, target_tokens, decoded_tokens = predict(
+            val_encoder, val_target, input_ctable, target_ctable,
+            maxlen, reverse, encoder_model, decoder_model, nb_tokens,
+            sample_mode=sample_mode, random=True)
+        
+        print('====================================')
+        print('Input tokens:  ', input_tokens)
+        print('Decoded tokens:', decoded_tokens)
+        print('Target tokens: ', target_tokens)
+        print('====================================')
+        
+        # Save the model at end of each epoch.
+        model_file = '_'.join(['seq2seq', 'epoch', str(epoch + 1)]) + '.h5'
+        model_dir = '/content'
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        save_path = os.path.join(model_dir, model_file)
+        print('Saving full model to {:s}'.format(save_path))
+        model.save(save_path)
