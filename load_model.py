1897b18308a2216a1c987cc862004759cdacb703
nursekti <59988090+nursekti@users.noreply.github.com>
2021-06-03 21:11:52 +0700

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -16,3 +16,37 @@ EOS = '*' # end of sequence.
 CHARS = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
 REMOVE_CHARS = '[#$%"\+@<=>!&,-.?:;()*\[\]^_`{|}~/\d\t\n\r\x0b\x0c]'
 
+class CharacterTable(object):
+    def __init__(self, chars):
+        self.chars = sorted(set(chars))
+        self.char2index = dict((c, i) for i, c in enumerate(self.chars))
+        self.index2char = dict((i, c) for i, c in enumerate(self.chars))
+        self.size = len(self.chars)
+    
+    def encode(self, C, nb_rows):
+        x = np.zeros((nb_rows, len(self.chars)), dtype=np.float32)
+        for i, c in enumerate(C):
+            x[i, self.char2index[c]] = 1.0
+        return x
+
+    def decode(self, x, calc_argmax=True):
+        if calc_argmax:
+            indices = x.argmax(axis=-1)
+        else:
+            indices = x
+        chars = ''.join(self.index2char[ind] for ind in indices)
+        return indices, chars
+      
+    def sample_multinomial(self, preds, temperature=1.0):
+        """Sample index and character output from `preds`,
+        an array of softmax probabilities with shape (1, 1, nb_chars).
+        """
+        # Reshaped to 1D array of shape (nb_chars,).
+        preds = np.reshape(preds, len(self.chars)).astype(np.float64)
+        preds = np.log(preds) / temperature
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
+        probs = np.random.multinomial(1, preds, 1)
+        index = np.argmax(probs)
+        char  = self.index2char[index]
+        return index, char
