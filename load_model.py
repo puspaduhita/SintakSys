601916e664b4b357e3a17d20908c2ac6ae738635
puspaduhita <49947654+puspaduhita@users.noreply.github.com>
2021-06-04 20:09:28 +0700

    Constructing Main

--- load_model.py
+++ load_model.py
@@ -223,3 +223,45 @@ def restore_model(path_to_full_model, hidden_size):
     decoder_model = Model(inputs=[decoder_inputs] + decoder_states_inputs,
                           outputs=[decoder_outputs] + decoder_states)
     return encoder_model, decoder_model
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0' # Using the GPU first visible to tensorflow for training
+
+# Initialize the Variable
+error_rate = 0.8
+reverse = True
+model_path = '/content/seq2seq_epoch_10.h5'
+hidden_size = 128
+sample_mode = 'argmax'
+data_path = '/content/'
+ref = ['ref.docx']
+uji = ['uji.docx']
+
+if __name__ == '__main__':
+    """ Data Processing """
+    text  = read_text(data_path, ref)
+    vocab = tokenize(text) # Tokenize the text
+    vocab = list(filter(None, set(vocab))) 
+    maxlen = max([len(token) for token in vocab]) + 2
+    train_encoder, train_decoder, train_target = transform(vocab, maxlen, error_rate=error_rate, shuffle=False)
+  
+    misspelled_tokens = []
+    test_sentence = read_text(data_path, uji)
+    tokens = tokenize(test_sentence)
+    tokens = list(filter(None, tokens))
+    nb_tokens = len(tokens)
+    for token in tokens:
+      token += EOS * (maxlen - len(token))
+      misspelled_tokens.append(token)
+
+    input_chars = set(' '.join(train_encoder))
+    target_chars = set(' '.join(train_decoder))
+    input_ctable = CharacterTable(input_chars)
+    target_ctable = CharacterTable(target_chars)
+    
+    encoder_model, decoder_model = restore_model(model_path, hidden_size)
+    
+    input_tokens, decoded_tokens = decode_sequences(misspelled_tokens, input_ctable, target_ctable,
+                                                    maxlen, reverse, encoder_model, decoder_model, nb_tokens,
+                                                    sample_mode=sample_mode, random=False)
+    
+    print('', ' '.join([token for token in decoded_tokens]))
